1b79024bf9c9da6d23b5473dac7d253cc222d74d
Galih Roswandi <galihroswandi25@gmail.com>
2023-05-22 22:27:37 +0700

    VIDEO 2 Installasi & Settup || VIDEO 3 Routing & Structure Folder

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen">
+      <h1>Dashboard</h1>
+      <Link href="/">Back</Link>
+    </div>
+  );
+};
+export default Dashboard;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Settings = () => {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-3xl">Settings Page</h1>
+      <Link href="/dashboard">Back To dashboard</Link>
+    </div>
+  );
+};
+
+export default Settings;
