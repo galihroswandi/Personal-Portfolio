8122a86b9a9317a2fa85d984963b8a052cd2ba35
Galih Roswandi <galihroswandi25@gmail.com>
2023-05-23 14:10:32 +0700

    VIDEO 10 Styling

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
+import styles from "./../styles.module.css";
+
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className={styles.dashboard}>
       <p>Layout For Dashboard</p>
       {children}
     </div>
