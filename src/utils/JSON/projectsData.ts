2a10648aef3a33742d9f42ec32ce8ca19bba65ec
galihroswandi <galihroswandi25@gmail.com>
2023-06-23 20:16:51 +0700

    Update Image

--- src/utils/JSON/projectsData.ts
+++ src/utils/JSON/projectsData.ts
@@ -5,7 +5,7 @@ export const projectsData = [
     typeWebsite: "Website E-comerce",
     description:
       "This simple e-commerce project uses ReactJS, Firebase, and Redux technologies. In this project, e-commerce applications are built using the ReactJS framework for a responsive user interface. Firebase is used as the backend for user authentication and data storage, while Redux is used for application state management. This combination of technologies enables the development of robust and responsive e-commerce applications.",
-    image: "/Projects/Project.png",
+    image: "/img/Projects/Project.png",
     linkDemo: "https://garudastoreindonesia.vercel.app/",
   },
 ];
