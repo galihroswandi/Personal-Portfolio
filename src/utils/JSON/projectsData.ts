91ee029f530a7afb53d2ac0b8510e8fd2179c181
galihroswandi <galihroswandi25@gmail.com>
2023-07-09 08:22:38 +0700

    Update Description Project

--- src/utils/JSON/projectsData.ts
+++ src/utils/JSON/projectsData.ts
@@ -13,7 +13,7 @@ export const projectsData = [
     title: "Personal website tiktok seller",
     typeWebsite: "Website Landing Page",
     description:
-      "This simple e-commerce project uses ReactJS, Firebase, and Redux technologies. In this project, e-commerce applications are built using the ReactJS framework for a responsive user interface. Firebase is used as the backend for user authentication and data storage, while Redux is used for application state management. This combination of technologies enables the development of robust and responsive e-commerce applications.",
+      "The TikTok Seller Client Website is a website project using Next.js and Tailwind CSS. This project aims to give online presence to TikTok sellers and introduce their products to TikTok users. Attractive and responsive landing pages will display concise and clear product information, using Call-to-Action (CTA) buttons to direct users to further actions. With Next.js and Tailwind CSS, this project provides a good user experience and makes it easier for TikTok sellers to introduce their products.",
     image: "/img/Projects/Project2.png",
     linkDemo: "https://baarshopid.vercel.app",
   },
