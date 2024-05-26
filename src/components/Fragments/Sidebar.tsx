c3fb3ff62a7573a8116d8894ecb1495bb6462474
galihroswandi <galihroswandi25@gmail.com>
2024-05-26 19:26:32 +0700

    fix: Sidebar margin

--- src/components/Fragments/Sidebar.tsx
+++ src/components/Fragments/Sidebar.tsx
@@ -8,7 +8,7 @@ import TextName from "../Elements/Sections/TextName";
 
 export default function Sidebar() {
   return (
-    <section className="fixed overflow-y-auto hidden md:flex md:flex-col md:gap-5 h-fit w-14 lg:w-64 py-2 lg:py-6 left-[10.3rem]">
+    <section className="fixed overflow-y-auto hidden md:flex md:flex-col md:gap-5 h-fit w-14 lg:w-64 py-2 lg:py-6 left-7 lg:left-14 xl:left-[10.2rem]">
       <section className="flex flex-col items-center gap-3 border-b border-slate-600 dark:border-slate-200 border-dashed h-fit py-3">
         <ProfileAvatar />
         <section className="flex flex-col items-center gap-2 lg:gap-0 lg:-mt-16">
